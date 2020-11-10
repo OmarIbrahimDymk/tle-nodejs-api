92c98f80128ed7386f0de322154786b3b1bee401
lord sayur <omar.ibrahim@dynamiktechnologies.com.bn>
2020-11-10 08:36:16 +0000

    add index

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require("express")();
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.json({ message: "hello" })
+})
+
+require('./app/routes/execution.routes')(app)
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
